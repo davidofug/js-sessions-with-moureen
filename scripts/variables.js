0d423eb88362091df94ae268fcb397253097feae
David Wampamba <davidwampamba@gmail.com>
2022-02-18 22:42:49 +0300

    Variable explanation and practice

---
+++ scripts/variables.js
@@ -0,0 +1,24 @@
+/**
+* A variable is unit of data storage
+* It is made up of three things;- the Identifier, the Operator and the Value.
+* The Identifier is the name of the variable.
+* In JavaScript the value determines the Data type of the variable.
+* Similar to other programming languages, JavaScript allows to Declare, Initialise or assign to variables.
+* Syntaxes
+* 1.Declaring a variable
+* keyword followed by the Identifier
+* We can use either var or let or const during declaration, and initialisation of variables.
+* let and const are recommended.
+*/
+
+let country; //Declaring a variable
+country = "Uganda"; //Assigning to a variable
+
+const city = "Kampala"; //Declaring and assigning to a variable or initializing a variable
+
+console.log(country); //Printing the value of a variable to browser console
+
+document.write(city); //Printing the value of a variable to the browser page
+
+console.log( typeof country ); //Printing the type of a variable to browser console
+
