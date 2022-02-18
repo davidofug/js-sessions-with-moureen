657c2eca14a7b96302b6c7868b7cec311227f3e1
David Wampamba <davidwampamba@gmail.com>
2022-02-18 23:11:35 +0300

    Data types

--- scripts/variables.js
+++ scripts/variables.js
@@ -20,5 +20,53 @@ console.log(country); //Printing the value of a variable to browser console
 
 document.write(city); //Printing the value of a variable to the browser page
 
-console.log( typeof country ); //Printing the type of a variable to browser console
+//Data Types
+// 1.String: A string is a sequence of characters. Strings are enclosed in double quotes or single quotes or backticks
+
+let first_name = 'David' //Single quotes
+let last_name = "Wampamba" //Double quotes
+let middle_name = `Melbourn` //Backticks
+console.log(typeof last_name);
+
+// 2.Number: A number is a sequence of digits.
+
+let age = 32; //Number || Integer
+let income = 750.95; //Number || Float
+let height = 5.0; //Number || Float
+
+console.log(typeof age);
+console.log(typeof income);
+console.log(typeof height);
+
+// 3.Boolean
+let is_married = false; //Boolean
+let has_seen = true; //Boolean
+console.log(typeof has_seen);
+
+// 4.Undefined
+let is_undefined; //Undefined
+console.log(typeof is_undefined);
+
+// 5.Object
+let is_null = null; //Null value object data type
+console.log(typeof is_null);
+
+let is_object = {} //Object value and data type
+console.log(typeof is_object);
+
+let is_array = [] //Array value and object data type
+console.log(typeof is_array);
+
+let is_array_2 = new Array() //Array value and object data type
+console.log(typeof is_array_2);
+
+let is_function = () => { }; //Function value and data type
+console.log(typeof is_function);
+function is_function_2() { }; //Function value and data type
+console.log(typeof is_function_2);
+
+let is_function_3 = function () { }; //Function value and data type
+console.log(typeof is_function_3);
+
+
 
