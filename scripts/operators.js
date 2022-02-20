3815d9b29276df81ce4faddee3aacaf251afe167
David Wampamba <davidwampamba@gmail.com>
2022-02-20 22:43:58 +0300

    Arithmetic operators

---
+++ scripts/operators.js
@@ -0,0 +1,26 @@
+/**
+ * Operators are grouped in the following categories.
+ * 1. Arithmetic operators
+ * 2. Comparison operators
+ * 3. Logical operators
+ * 4. Bitwise operators
+ * 5. Assignment operators
+ * 6. Increment/Decrement operators
+ * 7. Unary operators
+ * 8. Ternary operators
+ * 9. Special operators
+ *
+ */
+
+// Arithmetic Operators
+// +, -, *, /, %
+//Addition
+document.getElementById("addition_value").innerText = 4 + 9;
+//Subtraction
+document.getElementById("subtraction_value").innerText = 9 - 4;
+//Multiplication
+document.getElementById("multiplication_value").innerText = 4 * 9;
+//Division
+document.getElementById("division_value").innerText = 9 / 4;
+//Modulus
+document.getElementById("modulus_value").innerText = 9 % 4;
